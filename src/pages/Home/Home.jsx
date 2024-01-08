d4aabb814c6164eea31f3cbeb969400673b18c21
Guilherme Barros <gsbguilherme23@gmail.com>
2024-01-08 10:47:33 -0300

    add a pasta img e o conteudo da home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,13 @@
+import styles from "./Home.module.css"
+import ImgBanner from "../../img/img1.png"
+
 export default function Home(){
     return(
         <div>
-            Home
+            <h1>Bem vindo ao <span>FINANCE</span></h1>
+            <p>Com a Finance, você consegue criar e gerir seu projetos na área de técnologia.</p>
+            <p>Experimente, crie seu projeto agora mesmo!</p>
+            <img className={styles.imgHome} src={ImgBanner} alt="banner principal finance" />
         </div>
     )
 }
