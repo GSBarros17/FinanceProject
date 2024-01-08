415275fe539cdc87b1a111c93e2d5179ab631972
Guilherme Barros <gsbguilherme23@gmail.com>
2024-01-08 11:16:29 -0300

    add estilo a pg home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import ImgBanner from "../../img/img1.png"
 
 export default function Home(){
     return(
-        <div>
+        <div className={styles.containerHome}>
             <h1>Bem vindo ao <span>FINANCE</span></h1>
             <p>Com a Finance, você consegue criar e gerir seu projetos na área de técnologia.</p>
             <p>Experimente, crie seu projeto agora mesmo!</p>
