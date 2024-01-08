57193ffbdaf17a094c2c83333d77731e5a9a9885
Guilherme Barros <gsbguilherme23@gmail.com>
2024-01-08 10:34:11 -0300

    criando a rota da pgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home/Home"
 import './App.css'
 
 function App() {
 
   return (
     <>
-      hello
+      <div className="container">
+        <BrowserRouter>
+              <div className="container">
+                <Routes>
+                  <Route path="/" element={<Home/>}/>
+                </Routes>
+              </div>
+        </BrowserRouter>
+      </div>
     </>
   )
 }
