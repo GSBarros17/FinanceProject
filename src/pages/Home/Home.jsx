aa0c05fd6bb83740b3e61d6a9441484a709aaad9
Guilherme Barros <gsbguilherme23@gmail.com>
2024-01-10 11:38:19 -0300

    add link para criar um novo projeto na pg home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom"
 import styles from "./Home.module.css"
 import ImgBanner from "../../img/img1.png"
+
 
 export default function Home(){
     return(
@@ -7,6 +9,7 @@ export default function Home(){
             <h1>Bem vindo ao <span>FINANCE</span></h1>
             <p>Com a Finance, você consegue criar e gerir seu projetos na área de técnologia.</p>
             <p>Experimente, crie seu projeto agora mesmo!</p>
+            <Link to="/CreateProject" className="btnForm">Criar Projeto</Link>
             <img className={styles.imgHome} src={ImgBanner} alt="banner principal finance" />
         </div>
     )
