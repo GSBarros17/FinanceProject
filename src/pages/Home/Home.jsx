e3f914c3b3b73a144185b463a29525602551fb84
Guilherme Barros <gsbguilherme23@gmail.com>
2024-01-10 11:44:13 -0300

    add nome do usuario logado a pg home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom"
+import { useAuthValue } from "../../context/AuthContext"
 import styles from "./Home.module.css"
 import ImgBanner from "../../img/img1.png"
 
-
 export default function Home(){
+
+    const {user} = useAuthValue()
+    const firstName = user && user.displayName ? user.displayName.split(" ")[0] : "";
+    const firstNameCapital = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+
     return(
         <div className={styles.containerHome}>
-            <h1>Bem vindo ao <span>FINANCE</span></h1>
+            {firstNameCapital && <h1>{firstNameCapital}, bem vindo ao <span>FINANCE</span></h1>}    
+            {!firstNameCapital && <h1>Bem vindo ao <span>FINANCE</span></h1>}    
             <p>Com a Finance, você consegue criar e gerir seu projetos na área de técnologia.</p>
             <p>Experimente, crie seu projeto agora mesmo!</p>
-            <Link to="/CreateProject" className="btnForm">Criar Projeto</Link>
+            {user && <Link to="/CreateProject" className="btnForm">Criar Projeto</Link>}
             <img className={styles.imgHome} src={ImgBanner} alt="banner principal finance" />
         </div>
     )
