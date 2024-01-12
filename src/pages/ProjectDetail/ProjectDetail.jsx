72dbc364ce8b01ecb1870c0670f976842c8c5a4b
Guilherme Barros <gsbguilherme23@gmail.com>
2024-01-12 09:06:35 -0300

    criando a estrtura inicial da pg projectdetail

---
+++ src/pages/ProjectDetail/ProjectDetail.jsx
@@ -0,0 +1,9 @@
+import styles from "./ProjectDetail.module.css"
+
+export default function ProjectDetail(){
+    return(
+        <div className={styles.containerProjectDetail}>
+            <h1>Project Name</h1>
+        </div>
+    )
+}
